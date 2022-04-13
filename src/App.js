d00e704c1db65b13f69a9b7eadc0b1e39e5f179a
Sazzadur-Rahman-Morad <mdmouradsina@gmail.com>
2022-04-13 21:58:34 +0600

    All section and login and register section functionality are done

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import ServiceDetails from './Pages/ServiceDetails/ServiceDetails';
 import NotFound from './Pages/Shared/NotFound/NotFound';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import Checkout from './Pages/Checkout/Checkout';
+import RequireAuth from './Pages/Login/Login/RequireAuth/RequireAuth';
 function App() {
   return (
     <div>
@@ -19,6 +21,11 @@ function App() {
         <Route path='/about' element={<About></About>}></Route>
         <Route path='login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='checkout' element={
+          <RequireAuth>
+            <Checkout></Checkout>
+          </RequireAuth>
+        }></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
